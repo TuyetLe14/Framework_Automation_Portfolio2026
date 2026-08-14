039186aed676c506bd3e25481a2cf07de4b3215a
Tuyết Lê <62732180+TuyetLe14@users.noreply.github.com>
2026-08-14 16:45:29 +0700

    Update BaseTest.java

--- src/test/java/com/tuyet/base/BaseTest.java
+++ src/test/java/com/tuyet/base/BaseTest.java
@@ -1,13 +1,12 @@
 package com.tuyet.base;
 
-import com.tuyet.utils.*;
+import com.tuyet.utils.CaptureHelpers;
+import com.tuyet.utils.VideoRecorder;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import java.lang.reflect.Method;
-import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -18,40 +17,50 @@ public class BaseTest {
 
     @BeforeMethod
     public void createDriver(Method method) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        // Fix lỗi: Thêm headless nếu cần chạy CI/CD sau này
-        // options.addArguments("--headless=new");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        DriverManager.createDriver();
+        driver = DriverManager.getDriver();
 
-        if ("true".equals(System.getProperty("isRetry"))) {
-            System.out.println("🎥 ĐANG TÁI HIỆN BUG - QUAY VIDEO CHẬM...");
-            VideoRecorder.startRecording(method.getName() + "_Reproduce");
+        if ("true".equalsIgnoreCase(
+                System.getProperty("isRetry", "false"))) {
+
+            System.out.println(
+                    "🎥 ĐANG TÁI HIỆN BUG - QUAY VIDEO CHẬM..."
+            );
+
+            VideoRecorder.startRecording(
+                    method.getName() + "_Reproduce"
+            );
         }
     }
 
     @AfterMethod
     public void tearDown(ITestResult result) {
         String pageName = this.getClass().getSimpleName().replace("Test", "");
-
-        if (result.getStatus() == ITestResult.FAILURE) {
-                CaptureHelpers.takeScreenshot(driver, result.getName(), pageName);
-                System.out.println("📸 Đã chụp ảnh vị trí lỗi tại trang: " + pageName);
-            if (result.getMethod().getRetryAnalyzer(result) != null) {
-                VideoRecorder.stopAndKeepVideo();
-                System.out.println("🎥 Đã lưu video tái hiện bug.");
+        try {
+            if (result.getStatus() == ITestResult.FAILURE) {
+                if (drive != null) {
+                    CaptureHelpers.takeScreenshot(
+                            driver,
+                            result.getName(),
+                            pageName
+                    );
+                    System.out.println(
+                            "📸 Đã chụp ảnh vị trí lỗi tại trang: "
+                                    + pageName
+                    );
+                }
+                if (result.getMethod().getRetryAnalyzer(result) != null) {
+                    VideoRecorder.stopAndKeepVideo();
+                    System.out.println("🎥 Đã lưu video tái hiện bug.");
+                } else {
+                    VideoRecorder.stopAndDeleteVideo();
+                }
             } else {
-                VideoRecorder.stopAndDeleteVideo();
+              VideoRecorder.stopAndDeleteVideo();
             }
-        } else {
-            VideoRecorder.stopAndDeleteVideo();
-        }
-
-        if (driver != null) {
-            driver.quit();
+        } finally {
+            DriverManager.quitDriver();
+            driver = null;
         }
     }
 }
